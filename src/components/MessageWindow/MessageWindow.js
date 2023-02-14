7d46d8e07e511da8b4832b1f971714d7bcd3c790
benni maxon <benni.maxon@gmail.com>
2023-02-13 18:14:37 -0800

    styled chat window to improve readability

--- src/components/MessageWindow/MessageWindow.js
+++ src/components/MessageWindow/MessageWindow.js
@@ -4,13 +4,28 @@ import './MessageWindow.css';
 export default function MessageWindow() {
   return (
     <div className="message-window">
-      <p className="received">Hey</p>
-      <p className="sent">Hello</p>
-      <p className="received">How are you?</p>
-      <p className="sent">p good</p>
-      <p className="received">did u bring the updog?</p>
-      <p className="sent">what&apos;s updog?</p>
-      <p className="received">oh not much</p>
+      <div className="message-div div-received">
+        <p className="message received">Hey</p>
+      </div>
+      <div className="message-div div-sent">
+        <p className="message sent">Hello</p>
+      </div>
+      <div className="message-div div-received">
+        <p className="message received">How are you?</p>
+      </div>
+      <div className="message-div div-sent">
+        <p className="message sent">p good</p>
+      </div>
+      <div className="message-div div-received">
+        <p className="message received">did u bring the updog?</p>
+      </div>
+      <div className="message-div div-sent">
+        <p className="message sent">what&apos;s updog?</p>
+      </div>
+      <div className="message-div div-received">
+        <p className="message received">oh not much</p>
+      </div>
+      <div className="message-div div-sent"></div>
       <div className="chat-controls">
         <input className="message-input" type="text"></input>
         <button className="send-button">Send</button>
